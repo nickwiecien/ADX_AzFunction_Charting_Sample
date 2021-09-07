d68201c723c755179be28ddf950b6d99fc67e071
Nick Kwiecien <nikwieci@microsoft.com>
2021-09-06 23:04:13 -0500

    Updated readme and added sample image

--- CreateAnomalyChart/__init__.py
+++ CreateAnomalyChart/__init__.py
@@ -18,27 +18,45 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    # Parse request body and retrieve ADX query, cluster name, and database name
     req_body = req.get_json()
     query = req_body.get('query')
     cluster = req_body.get('cluster')
     database = req_body.get('database')
 
+    # For establishing connection to ADX retrieve service principal credentials (client id, tenant id, client secret)
+    # Details on granting service principal access to ADX can be found at:
+    # https://docs.microsoft.com/en-us/azure/data-explorer/manage-database-permissions
+    # Note: for storing/retrieving sensitive secrets in code it is recommended to store these values in Key Vault.
+    # https://docs.microsoft.com/en-us/azure/app-service/app-service-key-vault-references
     client_id = os.getenv('CLIENT_ID')
     tenant_id = os.getenv('TENANT_ID')
     client_secret = os.getenv('CLIENT_SECRET')
 
+    # Create connection to ADX using the azure-kusto-python python sdk
+    # https://github.com/Azure/azure-kusto-python/blob/master/azure-kusto-data/tests/sample.py
     kcsb = KustoConnectionStringBuilder.with_aad_application_key_authentication(cluster, client_id, client_secret, tenant_id)
     client = KustoClient(kcsb)
    
+    # Execute the user-provided query against ADX
     response = client.execute(database, query)
+
+    # Retrieve results in a Pandas dataframe
     df = dataframe_from_result_table(response.primary_results[0])
 
+    # Parse telemetry values, event times, and 'true_anomalies' from results
+    # Note: 'true_anomalies' in this instance are telemetry measurements identified as anomalous
+    # via the series_decompose_anomalies() function in ADX, AND fall outside of a user defined 
+    # range. 
+    # These headers are also dependent upon the provided query.
     telemetry_values = df['telemetry_series'][0]
     event_times = df['event_time'][0]
     true_anomalies = df['true_anomalies'][0]
 
+    # Format dates for charting with matplotlib
     dates = matplotlib.dates.date2num([datetime.strptime(x, '%Y-%m-%dT%H:%M:%S.%f0Z') for x in df['event_time'][0]])
 
+    # Identify all points which should be labeled as anomalies in final chart
     plot_anomalies = []
     plot_dates = []
 
@@ -47,6 +65,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             plot_anomalies.append(telemetry_values[idx])
             plot_dates.append(dates[idx])
 
+    # Generate anomalies chart (raw time series with anomalies annotated as red circles)
     fig, ax = plt.subplots(figsize= (13,6.5))
     ax.plot_date(dates,df['telemetry_series'][0], 'black', lw=1)
     
@@ -56,6 +75,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     ax.plot(plot_dates, plot_anomalies, 'red', lw=0, ms=7, marker='o')
     plt.title('Anomalies Detected')
 
+    # Chart is returned as a base64-encoded string. To generate this string we save a copy
+    # of the chart jpg locally, read and encode the bytes, then remove the temp file before
+    # returning a response
     tempdir = tempfile.gettempdir()
     temp_path = os.path.join(tempdir, 'output.jpg')
 
@@ -63,4 +85,5 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     b64_img_string = base64.b64encode(open(temp_path, 'rb').read()).decode()
     os.remove(temp_path)
 
+    # Return base64-encoded byte string
     return func.HttpResponse(b64_img_string, status_code=200)
--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # ADX_AzFunction_Charting_Sample
 
+This project contains code for an Azure Function which executes a user-provided query against Azure Data Explorer (ADX), generates an anomaly chart with Matplotlib, and returns as a base64-encoded byte string. This function was developed specifically for identifying anomalies in time-series data using a custom approach that finds anomalous measurements (identified as such using the series_decompose_anomalies() function in ADX) which fall outside of a user specified range. This function allows rapid creation of custom anomaly charts which can be consumed as part of an automated anomaly detection workflow.
+
+![Sample Anomaly Chart](img/anomaly_chart.jpg?raw=true "ADX Anomaly Chart")
+
 ## Environment Setup
-This function requires three environment variables (CLIENT_ID, CLIENT_SECRET, TENANT_ID) which map to a service principal that has database access in an ADX cluster.
+Before running this project, create a `local.settings.json` file in the root directory. This file needs to have the following entries under the `values` section:
+
+| Key                                 | Value                                    |
+|-------------------------------------|------------------------------------------|
+| AzureWebJobsStorage                 | The connection string to the storage account used by the Functions runtime.  To use the storage emulator, set the value to UseDevelopmentStorage=true |
+| FUNCTIONS_WORKER_RUNTIME            | Set this value to `python` as this is a python Function App |
+| CLIENT_ID | Client ID belonging to a service principal that has been granted access to the target ADX database. |
+| CLIENT_SECRET     | Client Secret belonging to the service principal referenced above. |
+| TENANT_ID     | Tenant ID belonging to the service principal referenced above. |
+
+For more instructions on configuring service principal access to ADX [please review the following document](https://docs.microsoft.com/en-us/azure/data-explorer/manage-database-permissions).
+
+When deploying this code to a Function App in Azure it is recommended to store sensitive keys and secrets in Azure Key Vault, and then reference these as environment variables in your app configuration. [See this document for more details](https://docs.microsoft.com/en-us/azure/app-service/app-service-key-vault-references).
